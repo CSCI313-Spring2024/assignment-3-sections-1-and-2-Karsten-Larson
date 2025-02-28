8b775f3d91c8a082ad6d2a028ee402b4d186587f
Karsten Larson <karsten.larson.1@gmail.com>
2025-02-28 12:05:22 -0600

    Completed Creating HomeComponent and added it to the Angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  imports: [RouterOutlet, HomeComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'assignment-3-homes-app';
+  title = "assignment-3-homes-app";
 }
