7639a2b14eee9623866dbb8df2b434a41d8f6883
Karsten Larson <karsten.larson.1@gmail.com>
2025-02-28 12:36:53 -0600

    Integrated details page into the application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,9 +8,10 @@ export const routes: Routes = [
     component: HomeComponent,
     title: 'Home page',
   },
-  {
-    path: 'details',
-    component: DetailsComponent,
-    title: 'Home Details',
-  },
+  // {
+  //   path: 'details',
+  //   component: DetailsComponent,
+  //   title: 'Home Details',
+  // },
+  { path: 'details/:id', component: DetailsComponent, title: 'Home Details' },
 ];
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../housinglocation';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [RouterModule, RouterLink, RouterOutlet],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css',
 })
