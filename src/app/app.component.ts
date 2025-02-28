3f0c1e8451b96d5695ff11aa792e4cf8235db8e3
Karsten Larson <karsten.larson.1@gmail.com>
2025-02-28 12:29:01 -0600

    Added routing to the angular application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
-  selector: "app-root",
-  imports: [RouterOutlet, HomeComponent],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.css",
+  selector: 'app-root',
+  imports: [RouterOutlet, HomeComponent, RouterModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = "assignment-3-homes-app";
+  title = 'assignment-3-homes-app';
 }
